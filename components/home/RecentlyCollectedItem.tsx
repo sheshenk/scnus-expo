33c5f094b747085a67b9244dea7531b874db0e85
Shashank Acharya <acharya.s@icloud.com>
2022-06-17 15:45:08 +0800

    changed image url getters

--- components/home/RecentlyCollectedItem.tsx
+++ components/home/RecentlyCollectedItem.tsx
@@ -2,10 +2,9 @@
 import { Card, Image } from "react-native-ui-lib"
 
 export default function RecentlyCollectedItem({item, onPress}) {
-	
 	return (
 		<Card marginR-15 style={{ width: 180, height: 180, borderRadius: 10, overflow: 'hidden'}} activeOpacity={0.9} onPress={onPress}>
-			<Image source={item.image} style={{width:'100%', height:'100%'}} resizeMode='contain'/>
+			<Image source={{uri: item.imageURL}} style={{width:'100%', height:'100%'}} resizeMode='contain'/>
 		</Card>
 	)
 }
