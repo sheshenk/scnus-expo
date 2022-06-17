4cc119600bdb7ffc82037af52ca9e6bd5bf63371
Shashank Acharya <acharya.s@icloud.com>
2022-06-17 13:11:31 +0800

    create settings page

---
+++ screens/Settings.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native-ui-lib"
+
+export default function Settings({navigation}) {
+	return (
+		<View>
+			<View paddingT-60 paddingB-20 paddingH-20 backgroundColor='grey'>
+				<Text color='white' text30>Settings</Text>
+			</View>
+		</View>
+	)
+}
