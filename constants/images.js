ac28858bb127eb6f1526ce9ebc23c7db56e2143f
Shashank Acharya <acharya.s@icloud.com>
2022-06-25 09:47:28 +0530

    updated image constants

--- constants/images.js
+++ constants/images.js
@@ -2,12 +2,18 @@ import banner from '../assets/images/banner.png'
 import back from '../assets/images/back.png'
 import browser from '../assets/images/browser.png'
 import share from '../assets/images/share.png'
+import logo from '../assets/images/logo.png'
+import nusflLogo from '../assets/images/nusfl-logo.png'
+import loginBackground from '../assets/images/login-background.png'
 
 const IMAGES = {
 	banner,
 	back,
+	logo,
 	browser,
-	share
+	share,
+	loginBackground,
+	nusflLogo
 }
 
 export default IMAGES
