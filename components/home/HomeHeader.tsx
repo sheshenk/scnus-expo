2af56bab7fc4f56f3f5999dad77a89254bdd48ba
Shashank Acharya <acharya.s@icloud.com>
2022-06-25 09:45:04 +0530

    adjusted header for undefined user and merchant

--- components/home/HomeHeader.tsx
+++ components/home/HomeHeader.tsx
@@ -1,18 +1,21 @@
+import { useContext } from "react"
 import { ImageBackground } from "react-native"
 import { Text, View } from "react-native-ui-lib"
 import IMAGES from "../../constants/images"
-import TOKENS from "../../constants/tokens"
+import { UserContext } from "../../services/userContext"
 import RecentlyCollectedGrid from "./RecentlyCollectedGrid"
 
-export default function HomeHeader({user}) {
+export default function HomeHeader() {
+	const {user, setUser} = useContext(UserContext)
+	const tokens = user ? user.tokens? user.tokens : [] : []
 	return (
-		<View style={{ width: '100%', height: 250 }}>
+		<View style={{ width: '100%', height: tokens.length > 0 ? 250 : 190 }}>
 			<ImageBackground source={IMAGES.banner} resizeMode='cover' style={{ flex: 1, alignItems: 'center'}}>
-				<View style={{marginTop: 70, width: '100%', alignItems: 'center', justifyContent: 'center'}}>
+				<View marginT-70 style={{width: '100%', alignItems: 'center', justifyContent: 'center'}}>
 					<Text color='white' text70>Welcome</Text>
-					<Text color='white' text30>{user.name}</Text>
+					<Text color='white' text30>{user ? user.name : ''}</Text>
 				</View>
-				<RecentlyCollectedGrid tokens={user.tokens.slice(0,3)}/>
+				{user && user.__typename === 'Customer' && tokens.length > 0 && <RecentlyCollectedGrid tokens={tokens.slice(0, 3)}/>}
 			</ImageBackground>
 		</View>
 	)
