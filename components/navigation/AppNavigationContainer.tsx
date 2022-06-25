859353603a8ccec156e1dc6be309ec9b2b6452e0
Shashank Acharya <acharya.s@icloud.com>
2022-06-25 09:47:40 +0530

    updated navigation for merchants

--- components/navigation/AppNavigationContainer.tsx
+++ components/navigation/AppNavigationContainer.tsx
@@ -1,29 +1,57 @@
-import { useQuery } from "@apollo/client"
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
-import { useEffect, useState } from "react"
-import { Text } from "react-native-ui-lib"
+import { useContext, useEffect } from "react"
 import Token from "../../screens/Token"
 import Tabs from "./Tabs"
-import { CURRENT_USER } from '../../queries/auth'
+import Auth from "../../screens/Auth"
+import { UserContext } from "../../services/userContext"
+import { useLazyQuery, useQuery } from "@apollo/client"
+import { CURRENT_USER } from "../../queries/auth"
+import { Text } from "react-native-ui-lib"
+import MerchantTabs from "./MerchantTabs"
+import Transaction from "../../screens/Transaction"
 
 export default function AppNavigationContainer() {
 	const Stack = createStackNavigator()
-	const [user, setUser] = useState(undefined)
-	const { loading, data } = useQuery(CURRENT_USER)
+	const {user, setUser} = useContext(UserContext)
+
+	const { data } = useQuery(CURRENT_USER)
 
 	useEffect(() => {
-		if (data && data.currentUser) setUser(data.currentUser)
-	}, [loading, data])
+		if (data) setUser(data.currentUser)
+	}, [data])
 
 	if (user === undefined) return <></>
-	if (user === null) return <Text>Login</Text>
 
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="App">
-				<Stack.Screen name='App' component={Tabs} options={{headerShown: false}}/>
-				<Stack.Screen name='Token' component={Token} options={{headerShown: false}}/>
+			<Stack.Navigator initialRouteName={user ? user.__typename === 'Merchant' ? 'MerchantApp' : 'App' : 'Auth'}>
+				<Stack.Screen name='Auth' options={{headerShown: false, animationEnabled: false, }}>
+					{(props) => <Auth {...props}/>}
+				</Stack.Screen>
+				{
+					user && user.__typename === 'Customer' &&
+						<Stack.Screen name='App' options={{headerShown: false}}>
+							{(props) => <Tabs {...props}/>}
+						</Stack.Screen>
+				}
+				{ user && user.__typename === 'Customer' &&
+					<Stack.Screen name='Token' options={{headerShown: false}}>
+						{(props) => <Token {...props}/>}
+					</Stack.Screen>
+				}
+				{
+					user && user.__typename === 'Merchant' &&
+						<Stack.Screen name='MerchantApp' options={{headerShown: false}}>
+							{(props) => <MerchantTabs {...props}/>}
+						</Stack.Screen>
+				}
+				{
+					user && user.__typename === 'Merchant' &&
+						<Stack.Screen name='Transaction' options={{headerShown: false}}>
+							{(props) => <Transaction {...props}/>}
+						</Stack.Screen>
+				}
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
